9db0b74eda40ba37211eb18ed00cf976d722f40a
oseiwus28 <oseiwusu28@gmail>
2023-10-16 10:31:21 +0000

    creating a QR code using React

---
+++ qrcode-react/src/app.jsx
@@ -0,0 +1,24 @@
+
+        function Scan() {
+            return (
+              <>
+                <div id="main-layout">
+                  <div id="content">
+                    <form id="text-form">
+                      <img id="image" src="/asset/css/main.css" alt="Main CSS" />
+                      <label htmlFor="text1">
+                        <b>Improve your front-end skills by building projects</b>
+                      </label>
+                      <label id="text2">
+                        Scan the QR Code to visit Frontend Mentor and take your coding skills to the next level.
+                      </label>
+                    </form>
+                  </div>
+                </div>
+              </>
+            );
+          }
+          
+    
+
+export default Scan;
---
+++ qrcode-react/src/page.jsx
@@ -0,0 +1,11 @@
+import Scan from "./app";
+
+function Page() {
+    return(
+        <BrowsweRouter>
+        <Scan/>
+        </BrowsweRouter>
+    )
+}
+
+export default Page;
